e158165de70adb274321b2b201a37878e04e9ccf
kheabat25 <kheabat@gmail.com>
2019-12-17 10:38:55 +0800

    Connect and Test connection of mysql database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'inventory',
+  debug: false
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/testconnect', (req, res, next) => {
+  if (db != null) {
+    res.send('Connection Success');
+  } else {
+    res.send('Connection Failed');
+  }
 });
 
 module.exports = router;
